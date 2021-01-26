3c45dfbedbebdbd8b5f4f598a6eb1af1d2a4d0f3
Weverton Paulino <wevertoncesar@gmail.com>
2021-01-26 18:25:39 -0300

    Adiciona recursos de log

--- EFCoreSample/Data/ApplicationContext.cs
+++ EFCoreSample/Data/ApplicationContext.cs
@@ -1,15 +1,22 @@
 using EFCoreSample.Domain;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 
 namespace EFCoreSample.Data
 {
     public class ApplicationContext : DbContext
     {
+        private static readonly ILoggerFactory _logger = LoggerFactory.Create(p => p.AddConsole());
+        public DbSet<Cliente> Clientes { get; set; }
         public DbSet<Pedido> Pedidos { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Data Source=(localdb)\\mssqllocaldb;Initial Catalog=EFCore;Integrated Security=true;");
+            optionsBuilder
+                .UseSqlServer("Data Source=(localdb)\\mssqllocaldb;Initial Catalog=EFCore;Integrated Security=true;")
+                .UseLoggerFactory(_logger)
+                .EnableSensitiveDataLogging();
 
             base.OnConfiguring(optionsBuilder);
         }
