d6c3b1a81859123b2407827fa697baf82daff3c6
Weverton Paulino <wevertoncesar@gmail.com>
2021-01-26 17:30:59 -0300

    Cria a primeira migracao

---
+++ EFCoreSample/PrimeiraMigracao.sql
@@ -0,0 +1,82 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+
+GO
+
+CREATE TABLE [Clientes] (
+    [Id] int NOT NULL IDENTITY,
+    [Nome] VARCHAR(100) NOT NULL,
+    [Telefone] CHAR(11) NOT NULL,
+    [Cep] CHAR(8) NOT NULL,
+    [Estado] CHAR(2) NOT NULL,
+    [Cidade] VARCHAR(100) NULL,
+    CONSTRAINT [PK_Clientes] PRIMARY KEY ([Id])
+);
+
+GO
+
+CREATE TABLE [Produtos] (
+    [Id] int NOT NULL IDENTITY,
+    [Descricao] VARCHAR(100) NOT NULL,
+    [Valor] decimal(18,2) NOT NULL,
+    [Tipo] nvarchar(max) NOT NULL,
+    [Ativo] bit NOT NULL,
+    CONSTRAINT [PK_Produtos] PRIMARY KEY ([Id])
+);
+
+GO
+
+CREATE TABLE [Pedidos] (
+    [Id] int NOT NULL IDENTITY,
+    [ClienteId] int NOT NULL,
+    [Emissao] datetime2 NOT NULL DEFAULT (GETDATE()),
+    [TipoFrete] nvarchar(max) NOT NULL,
+    [Status] nvarchar(max) NOT NULL,
+    [Observacao] nvarchar(250) NULL,
+    CONSTRAINT [PK_Pedidos] PRIMARY KEY ([Id]),
+    CONSTRAINT [FK_Pedidos_Clientes_ClienteId] FOREIGN KEY ([ClienteId]) REFERENCES [Clientes] ([Id]) ON DELETE CASCADE
+);
+
+GO
+
+CREATE TABLE [PedidoItens] (
+    [Id] int NOT NULL IDENTITY,
+    [PedidoId] int NOT NULL,
+    [ProdudoId] int NOT NULL,
+    [Quantidade] int NOT NULL DEFAULT 1,
+    [Valor] decimal(18,2) NOT NULL,
+    [Desconto] decimal(18,2) NOT NULL,
+    CONSTRAINT [PK_PedidoItens] PRIMARY KEY ([Id]),
+    CONSTRAINT [FK_PedidoItens_Pedidos_PedidoId] FOREIGN KEY ([PedidoId]) REFERENCES [Pedidos] ([Id]) ON DELETE CASCADE,
+    CONSTRAINT [FK_PedidoItens_Produtos_ProdudoId] FOREIGN KEY ([ProdudoId]) REFERENCES [Produtos] ([Id]) ON DELETE CASCADE
+);
+
+GO
+
+CREATE INDEX [idx_cliente_telefone] ON [Clientes] ([Telefone]);
+
+GO
+
+CREATE INDEX [IX_PedidoItens_PedidoId] ON [PedidoItens] ([PedidoId]);
+
+GO
+
+CREATE INDEX [IX_PedidoItens_ProdudoId] ON [PedidoItens] ([ProdudoId]);
+
+GO
+
+CREATE INDEX [IX_Pedidos_ClienteId] ON [Pedidos] ([ClienteId]);
+
+GO
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20210126184048_PrimeiraMigracao', N'3.1.5');
+
+GO
+
