43f088d709407777355f97e0b5999b9c0d8b2e07
Weverton Paulino <wevertoncesar@gmail.com>
2021-01-27 10:07:26 -0300

    Atualiza campo Produto na tabela PedidoItem

--- EFCoreSample/Domain/PedidoItem.cs
+++ EFCoreSample/Domain/PedidoItem.cs
@@ -5,8 +5,8 @@ namespace EFCoreSample.Domain
         public int Id { get; set; }
         public int PedidoId { get; set; }
         public Pedido Pedido { get; set; }
-        public int ProdudoId { get; set; }
-        public Produto Produdo { get; set; }
+        public int ProdutoId { get; set; }
+        public Produto Produto { get; set; }
         public int Quantidade { get; set; }
         public decimal Valor { get; set; }
         public decimal Desconto { get; set; }
---
+++ EFCoreSample/Migrations/20210127125059_AtualizaProduto.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EFCore.Migrations
+{
+    public partial class AtualizaProduto : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_PedidoItens_Produtos_ProdudoId",
+                table: "PedidoItens");
+
+            migrationBuilder.DropIndex(
+                name: "IX_PedidoItens_ProdudoId",
+                table: "PedidoItens");
+
+            migrationBuilder.DropColumn(
+                name: "ProdudoId",
+                table: "PedidoItens");
+
+            migrationBuilder.AddColumn<int>(
+                name: "ProdutoId",
+                table: "PedidoItens",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PedidoItens_ProdutoId",
+                table: "PedidoItens",
+                column: "ProdutoId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_PedidoItens_Produtos_ProdutoId",
+                table: "PedidoItens",
+                column: "ProdutoId",
+                principalTable: "Produtos",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_PedidoItens_Produtos_ProdutoId",
+                table: "PedidoItens");
+
+            migrationBuilder.DropIndex(
+                name: "IX_PedidoItens_ProdutoId",
+                table: "PedidoItens");
+
+            migrationBuilder.DropColumn(
+                name: "ProdutoId",
+                table: "PedidoItens");
+
+            migrationBuilder.AddColumn<int>(
+                name: "ProdudoId",
+                table: "PedidoItens",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PedidoItens_ProdudoId",
+                table: "PedidoItens",
+                column: "ProdudoId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_PedidoItens_Produtos_ProdudoId",
+                table: "PedidoItens",
+                column: "ProdudoId",
+                principalTable: "Produtos",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
